f6479500517fc84ac8d15a47ae9077d8c6f65789
Barathpalanisamy <barath@thirvusoft.in>
2022-06-20 17:08:45 +0530

    Sales Order Customize

---
+++ hydraulics/custom/custom_fields.py
@@ -0,0 +1,17 @@
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+def customize():
+    custom_fields = {
+        "Sales Order": [
+            dict(fieldname='abbr', label='Abbr',
+                fieldtype='Data', insert_after='column_break1', read_only=0),
+            dict(fieldname='additional_notes', label='Additional Notes',
+                fieldtype='Section Break', insert_after='items', read_only=0),
+            dict(fieldname='add_notes', label='Additional Notes',
+                fieldtype='Small Text', insert_after='items', read_only=0),
+        ]
+                }
+
+            
+    create_custom_fields(custom_fields)
+
+
